90f60140bd2d5e8a806505a547b5b8b5ad741434
argszero <argszero@gmail.com>
2015-03-20 16:44:55 +0800

    init

--- ProxyServer.java
+++ ProxyServer.java
@@ -103,17 +103,20 @@ class ProxyThread implements Runnable {
                 }
 
                 Map<String, List<String>> map = conn.getHeaderFields();
+                for(String k:new String[]{"Set-Cookie"}){
+
+                }
                 for (Map.Entry<String, List<String>> entry : map.entrySet()) {
                     for(String v:entry.getValue()){
                         System.out.println("Key : " + entry.getKey() +
                                 " ,Value : " + v);
                         out.write((entry.getKey()+":"+v).getBytes());
-                        out.write("\n".getBytes());
+                        out.write("\r".getBytes());
                     }
 
                 }
-                out.write("\n".getBytes());
-                out.write("\n".getBytes());
+                out.write("\r".getBytes());
+                out.write("\r".getBytes());
 
                 InputStream is = conn.getInputStream();
 
